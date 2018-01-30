4f7b4d9a77e2af36ca69499dc0e70d61b0aadc87
Joseph Howard <joe.howard@mac.com>
2018-01-29 22:00:05 -0500

    Populate db - wip

--- app/src/main/java/com/raywenderlich/android/roomword/WordApplication.kt
+++ app/src/main/java/com/raywenderlich/android/roomword/WordApplication.kt
@@ -43,7 +43,26 @@ class WordApplication : Application() {
 
   override fun onCreate() {
     super.onCreate()
-
     database = Room.databaseBuilder(this, WordRoomDatabase::class.java, "word_database").build()
   }
+
+//  private val roomDatabaseCallback = object : RoomDatabase.Callback() {
+//    override fun onOpen(db: SupportSQLiteDatabase) {
+//      super.onOpen(db)
+//      PopulateDbAsync(WordApplication.database).execute()
+//    }
+//  }
+//
+//  private class PopulateDbAsync(db: WordRoomDatabase) : AsyncTask<Void, Void, Void>() {
+//    private val dao: WordDao = db.wordDao()
+//
+//    override fun doInBackground(vararg params: Void): Void? {
+//      dao.deleteAll()
+//      var word = Word("Hello")
+//      dao.insert(word)
+//      word = Word("World")
+//      dao.insert(word)
+//      return null
+//    }
+//  }
 }
