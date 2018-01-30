9a316ac2c38547e9969a319dc65b9458641eaa4d
Joseph Howard <joe.howard@mac.com>
2018-01-29 20:22:39 -0500

    Create the DAO

--- app/src/main/java/com/raywenderlich/android/roomword/WordDao.kt
+++ app/src/main/java/com/raywenderlich/android/roomword/WordDao.kt
@@ -31,8 +31,19 @@
 
 package com.raywenderlich.android.roomword
 
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
 
-@Entity(tableName = "word_table")
-class Word(@PrimaryKey val word: String)
+@Dao
+interface WordDao {
+
+  @Insert
+  fun insert(word: Word)
+
+  @Query("DELETE FROM word_table")
+  fun deleteAll()
+
+  @Query("SELECT * FROM word_table ORDER BY word ASC")
+  fun getAllWords(): List<Word>
+}
